edf0a984bba570d44c2227c8ce25552839818351
yuki.yasuda <yuki.yasuda@jamstec.go.jp>
2025-09-30 10:52:12 +0000

    add attributes

--- src/configs/lorenz96_config.py
+++ src/configs/lorenz96_config.py
@@ -7,4 +7,13 @@ from src.frameworks.ddpm import DDPMConfig
 @dataclasses.dataclass()
 class ExperimentLorenz96Config(BaseConfig):
     batch_size: int
+    loss_name: str
+    learning_rate: float
+    #
+    n_features: int
+    list_channel: list[int]
+    #
+    total_epochs: int
+    save_interval: int
+    use_auto_mix_precision: bool
     ddpm: DDPMConfig
